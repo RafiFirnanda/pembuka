6819609acee6e167b0373852fa188a6852fa7a3d
RafiFirnanda <firnandarafi19@gmail.com>
2023-10-31 14:01:38 +0800

    first commit

---
+++ src/PersegiCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Program PersegiCalculator adalah program sederhana untuk menghitung luas dan keliling persegi.
+ */
+public class PersegiCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double sisi;
+
+        // Menampilkan pesan sambutan kepada pengguna
+        System.out.println("Program Hitung Luas dan Keliling Persegi");
+
+        // Meminta pengguna untuk memasukkan panjang sisi persegi
+        System.out.print("Masukkan panjang sisi persegi: ");
+        sisi = input.nextDouble();
+
+        // Menghitung luas persegi
+        double luas = sisi * sisi;
+
+        // Menghitung keliling persegi
+        double keliling = 4 * sisi;
+
+        // Menampilkan hasil luas persegi ke layar
+        System.out.println("Luas persegi: " + luas);
+
+        // Menampilkan hasil keliling persegi ke layar
+        System.out.println("Keliling persegi: " + keliling);
+    }
+}
